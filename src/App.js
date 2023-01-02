64b7d04060fe94a65844e2cc992be019e96ff298
Tejas-Naik <RNTejas2005@gmail.com>
2023-01-02 16:06:20 +0000

    Getting data from axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState({});
+
+  const getJoke = () => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then((response) => {
+        setJoke(response.data)
+      })
+  }
   return (
     <div className="App">
       <h1>Hello</h1>
+      <button onClick={getJoke}>Get Joke</button>
+      <h3>{joke.setup}</h3>
+      <h5>{joke.punchline}</h5>
     </div>
   );
 }
